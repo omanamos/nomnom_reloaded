9ba0e3fd714784cdf07567c28cbf3f16c2e0a0ce
Roy <roy@cs.washington.edu>
2010-11-09 12:12:55 -0800

    fixed up the fancybox and some other stuff

--- nomnom/public/javascripts/search.js
+++ nomnom/public/javascripts/search.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
-        $('input.viewtoggle').click(viewRecipeBody);
-        $('input.get_ingredients').click(chooseIngredients);
+	$('input.viewtoggle').click(viewRecipeBody);
+	
+	$('a.get_ingredients').fancybox({
+		'width'				: '75%',
+		'height'			: '75%',
+		'autoScale'			: false,
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'type'				: 'iframe'
+	}).addClass("blah");
+	
+	$("input#query").autocomplete({
+		source: getRecipeNames,
+		delay: 0
+	});
 });
 
-
+function getRecipeNames(request, callback) {
+	callback(["cookies", "ham", "spaghetti", "peanut butter cookie"]);	
+}
 
 function viewRecipeBody() {
     // access the JS DOM objects for the two text boxes on the page
@@ -16,9 +31,5 @@ function viewRecipeBody() {
         text.slideUp(500);
         this.value = "View Recipe";
     }
-}
-
-function chooseIngredients() {
-	$(this).fancybox();
 }
 
