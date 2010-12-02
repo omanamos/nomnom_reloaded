51f164ba5e3a60c92df1dcb76a0e61bf99a4afd8
omanamos <omanamos@gmail.com>
2010-12-01 23:22:06 -0800

    fix for autocomplete

--- nomnom/public/javascripts/application.js
+++ nomnom/public/javascripts/application.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 		return false;
 	});
 	
-	$("input#query").autocomplete('/search/show', { delay: 100, selectFirst: false });
+	$("input#query").autocomplete('/search/show', { delay: 100, selectFirst: true, autoFill: true });
 });
 
 function showLogin(){
