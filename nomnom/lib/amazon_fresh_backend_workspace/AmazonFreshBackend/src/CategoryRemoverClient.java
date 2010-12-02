609df71f11cb16fb911d6d6b275c03060b79af51
aryannaraghi <aryan.naraghi@gmail.com>
2010-12-01 22:57:39 -0800

    Finished auto-complete functionality.

---
+++ nomnom/lib/amazon_fresh_backend_workspace/AmazonFreshBackend/src/CategoryRemoverClient.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+
+/**
+ * 
+ * 
+ * @author Aryan Naraghi (aryan@cs.washington.edu)
+ */
+public class CategoryRemoverClient {
+
+	public static void main(String[] args) throws SolrServerException, IOException {
+		String solrUrl = args[0];
+		SolrServer solrServer = new CommonsHttpSolrServer(solrUrl);
+		
+		Scanner input = new Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Type in the category you would like to remove: ");
+			String category = input.nextLine();
+			solrServer.deleteByQuery("similarItems:\"" + category + "\"");
+			
+			if (category.isEmpty()) {
+				break;
+			}
+			
+		}
+		
+		System.out.print("Type cha-cha-slide to commit: ");
+		if (input.nextLine().equals("cha-cha-slide")) {
+			solrServer.commit();
+		}
+	}
+	
+}
