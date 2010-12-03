650675cd4a8d6c550c9848c60d0d30b0d2579e38
Roy <roy@cs.washington.edu>
2010-12-03 14:15:35 -0800

    added files

---
+++ nomnom/public/javascripts/backgroundChanger.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("body").randombg({
+            directory: "../../images/backgrounds/", //directory to image folder
+            howmany: 7 //how many images are in this folder     
+    });
+});
